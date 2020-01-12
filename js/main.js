f66af5f01f4c359e326006a79387402d4fe37be8
Morgan <m.a.henson@icloud.com>
2020-01-11 18:35:22 -0800

    file setup
    added jquery etc.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// variables (double check use of get element by id?)
+
+const keyboard = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+var missed = 0;
+
+// phrases array
+$('.btn__reset').on();
+
+String[] phrases = {};
